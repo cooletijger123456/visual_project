c7fa175151f452133622ac0b995c903d9a5bd21d
jameslahm <jameslahm17@gmail.com>
2025-02-13 10:17:23 +0800

    update

--- train_seg_vp.py
+++ train_seg_vp.py
@@ -44,7 +44,7 @@ for name, child in model.model.model[-1].named_children():
     if 'vpe' not in name:
         freeze.append(f"22.{name}")
 
-model.train(data=data, batch=128, epochs=5, **extends, close_mosaic=2, \
+model.train(data=data, batch=128, epochs=5, **extends, close_mosaic=5, \
     optimizer='AdamW', lr0=2e-3, warmup_bias_lr=0.0, \
         weight_decay=0.025, momentum=0.9, workers=4, \
         trainer=WorldSegVPTrainer, device='0,1,2,3,4,5,6,7', freeze=freeze, load_vp=True)
