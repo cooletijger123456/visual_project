eeeb734a54a2ef83d0f87e440c906c446ecce9f2
jameslahm <jameslahm17@gmail.com>
2025-02-18 16:00:59 +0800

    update

--- train_pe_free.py
+++ train_pe_free.py
@@ -13,11 +13,11 @@ data = dict(
         grounding_data=[
             dict(
                 img_path="../datasets/flickr/full_images/",
-                json_file="../datasets/flickr/annotations/final_flickr_separateGT_train.json",
+                json_file="../datasets/flickr/annotations/final_flickr_separateGT_train_segm.json",
             ),
             dict(
                 img_path="../datasets/mixed_grounding/gqa/images",
-                json_file="../datasets/mixed_grounding/annotations/final_mixed_train_no_coco.json",
+                json_file="../datasets/mixed_grounding/annotations/final_mixed_train_no_coco_segm.json",
             ),
         ],
     ),
@@ -35,8 +35,7 @@ extends = yaml_load(extend_cfg_path)
 assert(all(k in defaults for k in extends))
 LOGGER.info(f"Extends: {extends}")
 
-path = "yolov8l-worldv2-vlhead-mobileclip-ladapterglu-imgsz800-alpha1-segm-det1"
-model = YOLOWorld(f"{path}.pt")
+model = YOLOWorld("yolov8l-vl-seg-det.pt")
 
 # Ensure pe is set for classes
 names = ["object"]
@@ -51,7 +50,7 @@ for name, child in model.model.model[-1].named_children():
 
 freeze.extend(["22.cv3.0.0", "22.cv3.0.1", "22.cv3.1.0", "22.cv3.1.1", "22.cv3.2.0", "22.cv3.2.1"])
         
-model.train(data=data, batch=128, epochs=5, **extends, close_mosaic=5, \
+model.train(data=data, batch=128, epochs=1, **extends, close_mosaic=1, \
     optimizer='AdamW', lr0=2e-3, warmup_bias_lr=0.0, \
         weight_decay=0.025, momentum=0.9, workers=4, \
         trainer=WorldPEFreeTrainer, device='0,1,2,3,4,5,6,7', freeze=freeze, single_cls=True, train_pe_path=pe_path)
--- val_pe_free.py
+++ val_pe_free.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLOWorld
 from ultralytics.models.yolo.detect import DetectionValidator
 
-model = YOLOWorld("runs/detect/train38/weights/last.pt")
+model = YOLOWorld("runs/detect/train3/weights/last.pt")
 
 filename = "ultralytics/cfg/datasets/lvis.yaml"
 
-model.val(data=filename, batch=1, split='minival', rect=False, imgsz=800, validator=DetectionValidator, single_cls=True)
+model.val(data=filename, batch=1, split='minival', rect=False, validator=DetectionValidator, single_cls=True)
